7865aafa7847c3ce0a74a4a2b7494d7f9493e87d
Damien Darko <warwickjunglex@gmail.com>
2021-07-26 00:05:11 -0400

    Initial commit

---
+++ game.js
@@ -0,0 +1,8 @@
+// initialize kaboom context
+kaboom({
+    global: true, // so you don't have to use kaboom.loadSprite etc
+    crisp: true, // for crisp pixel art
+    width: 320, // width of canvas
+    height: 240, // height of canvas
+    scale: 3 // scales the game up
+});
